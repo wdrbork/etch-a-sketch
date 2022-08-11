d635a06d321273a85bb0f7ebdae34ae83828f4cb
wdrbork <wdrb2001@gmail.com>
2022-08-11 01:21:06 -0700

    Implement sketch feature on the grid

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ for (let i = 0; i < 256; i++) {
     sketchpad.appendChild(div);
 }
 
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+        square.classList.toggle('black-square');
+    })
+});
